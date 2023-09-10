35854b7f9acf17d203b16345c7003876ff91ec4a
VictorDamian <vdamianbarron@gmail.com>
2023-09-09 18:47:20 -0600

    Agregando controlador para login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspMvcEF.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Enter(string user, string pass)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                //COntent regresa un strin en vez de vista
+                return Content("Ocurrio un error" + ex.Message);
+            }
+        }
+    }
+}

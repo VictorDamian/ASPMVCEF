653385152c59bad97a500ebbf17a5ccde4a54171
VictorDamian <vdamianbarron@gmail.com>
2023-10-05 17:35:25 -0600

    Metodo para cerrar sesion

---
+++ Controllers/CloseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspMvcEF.Controllers
+{
+    public class CloseController : Controller
+    {
+        // GET: Close
+        public ActionResult Logout()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
